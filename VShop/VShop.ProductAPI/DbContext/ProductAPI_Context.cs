33cfbbb4c02d45b40a75770cc717d86873336e0f
Guilherme Reichert <GuiReichert.TR@gmail.com>
2025-02-20 18:21:09 -0300

    Creating Project and First Microsservice: 'ProductAPI'

---
+++ VShop/VShop.ProductAPI/DbContext/ProductAPI_Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductAPI.Models;
+
+namespace VShop.ProductAPI.ProductAPI_Context;
+
+public class ProductAPI_Context : DbContext
+{
+    public ProductAPI_Context(DbContextOptions options) : base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+
+}
